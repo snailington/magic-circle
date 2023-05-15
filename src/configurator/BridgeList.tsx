a91e1281dc6c186a7aed731f8948d121cccc3a0d
snail <homeless@snail.moe>
2023-05-15 02:53:46 -0400

    unmocked bridge list

--- src/configurator/BridgeList.tsx
+++ src/configurator/BridgeList.tsx
@@ -1,13 +1,13 @@
 import BridgeListItem from "./BridgeListItem.tsx";
 import "./BridgeList.css"
+import {getBridges} from "../BridgeConfig.ts";
+import OBR from "@owlbear-rodeo/sdk";
 
 export default function BridgeList() {
     return (
         <div id="bridge-list">
-            <BridgeListItem />
-            <BridgeListItem />
-            <BridgeListItem />
-            <BridgeListItem />
+            { getBridges(OBR.room.id)?.map((b) =>
+                <BridgeListItem key={b.name} bridge={b} />) }
         </div>
     )
 }
--- src/configurator/BridgeListItem.tsx
+++ src/configurator/BridgeListItem.tsx
@@ -1,11 +1,24 @@
 import "./BridgeListItem.css"
+import {BridgeConfig} from "../BridgeConfig.ts";
+import {useEffect, useState} from "react";
+import {BridgeStatusClient} from "../BridgeStatus.ts";
 
-export default function BridgeListItem() {
+export default function BridgeListItem({bridge}: {bridge: BridgeConfig }) {
+    const [activity, setActivity] = useState(false);
+
+    useEffect(() => BridgeStatusClient.onUpdate(bridge.name, () => {
+        setActivity(true);
+
+        setTimeout(() => setActivity(false), 1000);
+    }), []);
+
+    const statusClass = activity ? "status-active" : "status-inactive";
+
     return (
         <div className="bridge">
-            <div className="bridge-name">Bridge Name</div>
-            <div className="bridge-type">Bridge Type</div>
-            <div className="bridge-status" />
+            <div className="bridge-name">{bridge.name}</div>
+            <div className="bridge-type">{bridge.type}</div>
+            <div className={`bridge-status ${statusClass}`} />
             <button className="btn-bridge-open">open</button>
             <button className="btn-bridge-edit">edit</button>
             <button className="btn-bridge-delete">X</button>
