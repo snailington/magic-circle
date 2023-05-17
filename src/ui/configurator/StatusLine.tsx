6068586eca63f0da3d2d2fb52123468ec7e044f8
snail <homeless@snail.moe>
2023-05-17 01:37:21 -0400

    control panel status line message

--- src/ui/configurator/StatusLine.tsx
+++ src/ui/configurator/StatusLine.tsx
@@ -1,9 +1,21 @@
+import {useEffect, useState} from "react";
 import "./StatusLine.css"
+import {BridgeStatusClient} from "../../BridgeStatus.ts";
 
 export default function StatusLine() {
+    const [log, setLog] = useState("");
+
+    useEffect(() => BridgeStatusClient.onGlobal((bridge: string, cmd: string) => {
+        const now = new Date();
+        const time = [now.getHours(), now.getMinutes(), now.getSeconds()]
+            .map((t) => t.toString().padStart(2, '0'))
+            .join(':');
+        setLog(`${time}: ${bridge} → ${cmd}`);
+    }), []);
+
     return (
         <div id="status-line">
-            
+            {log}
         </div>
     );
 }
