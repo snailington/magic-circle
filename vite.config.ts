df0838c71f2c87e8ebe43c486d40f5845f91b834
snail <homeless@snail.moe>
2023-05-17 05:33:15 -0400

    reorganized build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "background.html"),
+        newbridge: resolve(__dirname, "newbridge.html"),
+        deletebridge: resolve(__dirname, "deletebridge.html")
+      }
+    }
+  }
 })
